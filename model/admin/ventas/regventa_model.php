2a927dcbd9eb7e3d6c6a099a3a6b39efc71a6e2e
amayadaniel677 <amayadaniel677@gmail.com>
2024-04-23 11:00:33 -0500

    nueva BD y ajusten en regventa model
    cambio la bd de regventa entonces se cambian las columnas del modelo

--- model/admin/ventas/regventa_model.php
+++ model/admin/ventas/regventa_model.php
@@ -1,11 +1,13 @@
-<?php 
-class RegVenta_consult{
+<?php
+class RegVenta_consult
+{
 
     private $con;
 
-    public function __construct() {
+    public function __construct()
+    {
         mysqli_report(MYSQLI_REPORT_STRICT | MYSQLI_REPORT_ERROR);
-    
+
         try {
             $this->con = new mysqli("localhost", "edutech", "edutechadso2024", "edutech");
         } catch (mysqli_sql_exception $e) {
@@ -18,11 +20,12 @@ class RegVenta_consult{
                 // Considera lanzar una excepción o manejar el error de otra manera
             }
         }
-    
+
         $this->con->set_charset("utf8");
     }
 
-    public function nombre_area(){
+    public function nombre_area()
+    {
         $sql = "SELECT name FROM areas";
         $result = $this->con->query($sql);
 
@@ -39,157 +42,156 @@ class RegVenta_consult{
         } else {
             return array('error' => true, 'message' => "La consulta no devolvió resultados.");
         }
-    
     }
-    public function traer_modalidades(){
-        $sql="SELECT * FROM modalities WHERE status='active'";
-        $result=$this->con->query($sql);
-        if($result->num_rows>0){
-            $modalidades=array();
-            while($row=$result->fetch_assoc()){
+    public function traer_modalidades()
+    {
+        $sql = "SELECT * FROM modalities WHERE status='active'";
+        $result = $this->con->query($sql);
+        if ($result->num_rows > 0) {
+            $modalidades = array();
+            while ($row = $result->fetch_assoc()) {
                 // guardar en un array cada fila de la consulta
-                $modalidades[]=$row;
+                $modalidades[] = $row;
             }
             return $modalidades;
-        }else{
+        } else {
             return false;
         }
     }
-    public function obtenerIdAreas($nombreArea){
-        $sql="SELECT id FROM areas Where name='$nombreArea'";
-        $result=$this->con->query($sql);
-        if($result->num_rows > 0 ){
-            while($row=$result->fetch_assoc()){
-               $categoriaId= $row['id'];
+    public function obtenerIdAreas($nombreArea)
+    {
+        $sql = "SELECT id FROM areas Where name='$nombreArea'";
+        $result = $this->con->query($sql);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $categoriaId = $row['id'];
             }
             return $categoriaId;
         }
     }
 
-    public function nombre_subject($categoria){
-        $id=$this->obtenerIdAreas($categoria);
-        $sql="SELECT name FROM subjects WHERE areas_id='$id'";
-        $result=$this->con->query($sql);
-        if($result->num_rows > 0){
-            $nombres_subjects=array();
-            while($row=$result->fetch_assoc()){
-                $nombres_subjects[]=$row['name'];
+    public function nombre_subject($categoria)
+    {
+        $id = $this->obtenerIdAreas($categoria);
+        $sql = "SELECT name FROM subjects WHERE areas_id='$id'";
+        $result = $this->con->query($sql);
+        if ($result->num_rows > 0) {
+            $nombres_subjects = array();
+            while ($row = $result->fetch_assoc()) {
+                $nombres_subjects[] = $row['name'];
             }
-           
-            return $nombres_subjects ;
-        }else{
-            $error_message[]='Base de datos con campos vacíos';
+
+            return $nombres_subjects;
+        } else {
+            $error_message[] = 'Base de datos con campos vacíos';
             return $error_message;
         }
     }
 
-    public function precio_area($categoria){
-        $sql="SELECT price FROM areas WHERE name='$categoria'";
-        $result=$this->con->query($sql);
+    public function precio_area($categoria)
+    {
+        $sql = "SELECT price FROM areas WHERE name='$categoria'";
+        $result = $this->con->query($sql);
 
-        if($result->num_rows > 0){
-            while($row=$result->fetch_assoc()){
-                $precio_area=$row['price'];
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $precio_area = $row['price'];
             }
             return $precio_area;
-        }else{
-            $precio_area[]='Base de datos con campos vacíos';
+        } else {
+            $precio_area[] = 'Base de datos con campos vacíos';
             return $precio_area;
         }
     }
 
     // METODOS PARA AGREGAR LA VENTA
-    public function agregar_venta_completa($nombres,$apellidos,$dni,$correo,$ciudad,$telefono,$descuento,$valor_total){
-    //    si existe el usuario
-    $user_exist=$this->user_exist($dni);
-    if(!$user_exist){
-       $registrar=$this->user_register($nombres,$apellidos,$dni,$correo,$ciudad,$telefono);
-       $user_exist=$this->user_exist($dni);
-    }
-    if($user_exist){
-        $date = date("Y-m-d");
-        $sql="INSERT INTO sales(price,`date`,people_id) VALUES('$valor_total','$date','$user_exist')";
-        $result=$this->con->query($sql);
-        if($result){
-            // Obtener el ID de la última inserción
-            $lastInsertedId = $this->con->insert_id;
-            return $lastInsertedId;
+    public function agregar_venta_completa($nombres, $apellidos, $dni, $correo, $ciudad, $telefono, $descuento, $valor_total)
+    {
+        //    si existe el usuario
+        $user_exist = $this->user_exist($dni);
+        if (!$user_exist) {
+            $registrar = $this->user_register($nombres, $apellidos, $dni, $correo, $ciudad, $telefono);
+            $user_exist = $this->user_exist($dni);
         }
-        else{
+        if ($user_exist) {
+            $date = date("Y-m-d");
+            $sql = "INSERT INTO sales(price,`date`,people_id) VALUES('$valor_total','$date','$user_exist')";
+            $result = $this->con->query($sql);
+            if ($result) {
+                // Obtener el ID de la última inserción
+                $lastInsertedId = $this->con->insert_id;
+                return $lastInsertedId;
+            } else {
+                return false;
+            }
+        } else {
             return false;
         }
-    }else{
-        return false;
     }
-
-
-    }
-    public function user_exist($dni){
-        $sql="SELECT * FROM people WHERE dni='$dni'";
-        $result=$this->con->query($sql);
-        if($result->num_rows>0){
-            $row=$result->fetch_assoc();
+    public function user_exist($dni)
+    {
+        $sql = "SELECT * FROM people WHERE dni='$dni'";
+        $result = $this->con->query($sql);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
             return  $row['id'];
-        }else{
+        } else {
             return false;
         }
     }
-    public function user_register($nombres,$apellidos,$dni,$correo,$ciudad,$telefono){
-        $contrasenia_encriptada=password_hash($dni,PASSWORD_DEFAULT,['cost'=>10]);
-        $rol='estudiante';
-        $dni_type='NA';
-        $foto='resource/img/photosUsers/defaultPhoto.png';
-        $sql="INSERT INTO people(`name`,lastname,dni,email,city,phone,`password`,rol,dni_type,photo) VALUES('$nombres','$apellidos','$dni','$correo','$ciudad','$telefono','$contrasenia_encriptada','$rol','$dni_type','$foto')";
-        $result=$this->con->query($sql);
-        if($result){
+    public function user_register($nombres, $apellidos, $dni, $correo, $ciudad, $telefono)
+    {
+        $contrasenia_encriptada = password_hash($dni, PASSWORD_DEFAULT, ['cost' => 10]);
+        $rol = 'estudiante';
+        $dni_type = 'NA';
+        $foto = 'resource/img/photosUsers/defaultPhoto.png';
+        $sql = "INSERT INTO people(`name`,lastname,dni,email,city,phone,`password`,rol,dni_type,photo) VALUES('$nombres','$apellidos','$dni','$correo','$ciudad','$telefono','$contrasenia_encriptada','$rol','$dni_type','$foto')";
+        $result = $this->con->query($sql);
+        if ($result) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-    public function agregar_detalles_venta($detallesVenta,$sales_id){
+    public function agregar_detalles_venta($detallesVenta, $sales_id)
+    {
         $cantidadDetalles = count($detallesVenta);
-        $exitoDetalles=0;
-        foreach($detallesVenta as $detalle){
-            $price=$detalle['valorCurso'];
-           
-            var_dump($detalle);
-            $modality=$detalle['modalidad'];
-            $tipo_venta=$detalle['tipoVenta'];
-            if($tipo_venta=='horas'){
-                $total_hours=$detalle['cantidad_horas_clases'];
-                $total_classes=0;
-            }elseif($tipo_venta=='clases'){
-                $total_classes=$detalle['cantidad_horas_clases'];
-                $total_hours=0;
-            }
-            $subjects_id=$this->obtener_id_curso($detalle['curso']);
-            $sql="INSERT INTO subject_sale(price,total_hours,subjects_id,sales_id,remaining_hours,modality,quantity_type,total_classes,remaining_classes) VALUES('$price','$total_hours','$subjects_id','$sales_id','$total_hours','$modality','$tipo_venta','$total_classes','$total_classes') ";
-            $result=$this->con->query($sql);
-            if($result){
+        $exitoDetalles = 0;
+        foreach ($detallesVenta as $detalle) {
+            $price = $detalle['valorCurso'];
+            $modality = $detalle['modalidad'];
+            $tipo_venta = $detalle['tipoVenta'];
+            $total_quantity = $detalle['cantidad_horas_clases'];
+
+            $subjects_id = $this->obtener_id_curso($detalle['curso']);
+            $sql = "INSERT INTO subject_sale(price,subjects_id,sales_id,modality,quantity_type,total_quantity) VALUES('$price','$subjects_id','$sales_id','$modality','$tipo_venta','$total_quantity') ";
+            $result = $this->con->query($sql);
+            if ($result) {
                 $exitoDetalles += 1;
+                // falta agregar en la nueva tabla de remaining_units la nueva cantidad de horas restantes
+                //falta validar si existe otro detalle que pertenezca a la misma persona y con el mismo curso y el mismo tipo (horas-clases)
+                //si pertenece a la misma persona y mismo curso entonces se usa la misma fk de remaining_units
+                //si no pertenece a la misma persona y mismo curso entonces se crea un nuevo registro en remaining_units
             }
         }
-        if($cantidadDetalles == $exitoDetalles){
+        if ($cantidadDetalles == $exitoDetalles) {
             return true;
-        }else{
+        } else {
             false;
         }
-
     }
-    public function obtener_id_curso($curso){
-        $sql="SELECT id FROM subjects WHERE `name`='$curso' ";
-        $result=$this->con->query($sql);
-        if($result->num_rows>0){
-     // Utilizamos fetch_assoc para obtener un array asociativo
-        $fila = $result->fetch_assoc();
-        // Accedemos al valor de la columna 'id'
-        $id = $fila['id'];
-        return $id;
-        }else{
+    public function obtener_id_curso($curso)
+    {
+        $sql = "SELECT id FROM subjects WHERE `name`='$curso' ";
+        $result = $this->con->query($sql);
+        if ($result->num_rows > 0) {
+            // Utilizamos fetch_assoc para obtener un array asociativo
+            $fila = $result->fetch_assoc();
+            // Accedemos al valor de la columna 'id'
+            $id = $fila['id'];
+            return $id;
+        } else {
             return false;
         }
     }
 }
-
-?>
