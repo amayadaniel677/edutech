933ea5d554104c39693f57b0ee942adabada9936
amayadaniel677 <amayadaniel677@gmail.com>
2024-05-09 08:16:23 -0500

    cambios daniel breadcrumbs agregar curso

--- view/admin/paginas/cursos/agregar_curso.php
+++ view/admin/paginas/cursos/agregar_curso.php
@@ -51,8 +51,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </div><!-- /.col -->
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active">Starter Page</li>
+                <li class="breadcrumb-item"><a href="../controller_inicio_admin.php">Inicio</a></li>
+                <li class="breadcrumb-item"><a href="./controller_gestionar_cursos">Cursos</a></li>
+                <li class="breadcrumb-item active">Agregar curso</li>
               </ol>
             </div><!-- /.col -->
           </div><!-- /.row -->
