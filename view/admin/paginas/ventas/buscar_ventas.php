7cb43b7ef55a794fc5e5facdbe87cd029ea9d87c
daamaya22 <daamaya@unal.edu.co>
2024-05-24 17:27:37 -0500

    spanglish en datables

--- view/admin/paginas/ventas/buscar_ventas.php
+++ view/admin/paginas/ventas/buscar_ventas.php
@@ -333,14 +333,23 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <script src="../../../view/admin/plugins/datatables/jquery.dataTables.min.js"></script>
 
   <script src="../../../view/admin/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+
   <script src="../../../view/admin/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+
   <script src="../../../view/admin/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+
   <script src="../../../view/admin/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+
   <script src="../../../view/admin/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+
   <script src="../../../view/admin/plugins/jszip/jszip.min.js"></script>
+
   <script src="../../../view/admin/plugins/pdfmake/pdfmake.min.js"></script>
+
   <script src="../../../view/admin/plugins/pdfmake/vfs_fonts.js"></script>
+
   <script src="../../../view/admin/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+
   <script src="../../../view/admin/plugins/datatables-buttons/js/buttons.print.min.js"></script>
   <script src="../../../view/admin/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
   <!-- AdminLTE App -->
