20a6ee1c4da0b4cfe735359267b1ea11143f5e5b
Martinhurtad0 <140752914+Martinhurtad0@users.noreply.github.com>
2024-05-24 17:31:39 -0500

    cambio login

--- view/login.php
+++ view/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar secion</title>
-    <link rel="stylesheet" href="../resource/css/login/login.css">
+    <link rel="stylesheet" href="../resource/css/login/login1.css">
     
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
