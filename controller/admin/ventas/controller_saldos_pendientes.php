3f0282540b6c66ae05b9d312ab5b1efaa9160ae4
amayadaniel677 <amayadaniel677@gmail.com>
2024-05-29 08:35:10 -0500

    saldos pendientes terminado
    falta solo visualizar los abonos realizados

--- controller/admin/ventas/controller_saldos_pendientes.php
+++ controller/admin/ventas/controller_saldos_pendientes.php
@@ -11,4 +11,29 @@ include('../../../model/admin/ventas/saldos_pendientes_model.php');
 // LOGICA
 $consulta = new saldos_pendientes_model();
 $abonos_pendientes = $consulta->traer_abonos_pendientes();
+if(isset($_GET['mensaje_ok'])){
+    $mensaje_ok=$_GET['mensaje_ok'];
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['saldo_id'])) {
+    $saldo_id = $_POST['saldo_id'];
+    $valor_abono = $_POST['valor_abono'];
+    $valor_abono_2= $_POST['valor_abono_2'];
+    $modelo = new saldos_pendientes_model();
+    if($valor_abono==$valor_abono_2){
+        $resultado_modelo=$modelo->abonar_saldo($saldo_id, $valor_abono);
+        if($resultado_modelo==1){
+            $mensaje_error='';
+            $mensaje_ok="Abono registrado correctamente";
+            header('location:controller_saldos_pendientes.php?mensaje_ok='.$mensaje_ok);
+        }else{
+            $mensaje_ok='';
+            $mensaje_error= $resultado_modelo;
+        }
+    }else{
+        $mensaje_ok='';
+        $mensaje_error="Error: los valores de abono no coinciden";  
+    
+    }
+   
+} 
 include('../../../view/admin/paginas/ventas/saldos_pendientes.php');
--- view/admin/paginas/ventas/saldos_pendientes.php
+++ view/admin/paginas/ventas/saldos_pendientes.php
@@ -92,10 +92,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <section class="content">
 
                 <button id="btnInfo" type="button" class="btn btn-success swalDefaultSuccess" style="display:none ">
-                    error
+                 
                 </button>
                 <button id="btnError" type="button" class="btn btn-success swalDefaultError" style="display:none ">
-                    error
+             
+                </button>
+                <button id="btnSuccess" type="button" class="btn btn-success swalDefaultSuccess" style="display:none ">
+                 
                 </button>
                 <div class="container-fluid" style="max-width:1300px;">
 
@@ -125,24 +128,24 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                     </thead>
                                     <tbody>
                                         <?php
-                                        if (isset($abonos_pendientes)) {
+                                        if (isset($abonos_pendientes) and !empty($abonos_pendientes) ){
 
                                             foreach ($abonos_pendientes as $abono) {
                                                 echo '<tr>
                       
                               <td>' . $abono['fecha_venta'] . '</td>
-                              <td>' . $abono['id_venta'] . '</td>
+                              <td>' . $abono['id_venta']. '</td>
                               <td>' . $abono['person_name'], " ", $abono['person_lastname'] . '</td>
                               <td>' . $abono['valor_venta'] . '</td>
                               <td>' . $abono['valor_abonado'] . '</td>
                               <td>' . $abono['valor_venta'] - $abono['valor_abonado'] . '</td>
                               <td>' . $abono['ultimo_abono'] . '</td>
                               <td>
-                              <a href="controller_detalle_abonos.php?id_venta=' . $abono['id_venta'] . '" class="btn btn-primary" data-toggle="tooltip" title="Registro de abonos">
-                              <i class="fas fa-eye"></i>
+                              <a class="btn btn-outline-success abrir-modal-abonos" data-target="#modalAbonos" data-id-saldo="'.$abono['id_saldo'].'" data-toggle="tooltip" data-placement=""top" title="Abonos realizados">
+                              <i class="fas fa-plus"></i>
                               </a>
                           
-                              <a href="#" onclick="confirmarEliminarVenta(\'' . 'controller_eliminar_venta.php?id_venta=' . $abono['id_venta'] . '\')" class="btn btn-danger" data-toggle="tooltip" title="Eliminar" id="desactivarVenta"><i class="fas fa-trash"></i></a>
+                              <a href="controller_detalle_abonos.php?id_venta=' . $abono['id_venta'] . '"   class="btn btn-primary" data-toggle="tooltip" title="Ver abonos realizados" id="desactivarVenta"><i  class="fas fa-eye"></i></a>
                                
 
                               </td>
@@ -155,7 +158,44 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                         // Cierra la tabla HTML
 
                                         ?>
+ <!-- Modal -->
+ <div class="modal fade" id="modalAbonos" tabindex="-1" role="dialog" aria-labelledby="miModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h3 class="modal-title" id="miModalLabel">Nuevo abono</h3>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <!-- Aquí irá el contenido dinámico del modal -->
+                                                    <!-- Agrega el ID "formPagar" al formulario -->
+                                                    <form id="formAddAbono" method="POST" action="">
+                                                        <div class="">
+                                                            <input type="hidden" name="saldo_id" id="saldo_id">
+                                                            <div class="col-md-12 mt-3">
+                                                                <label for="valor_abono">Valor del abono</label>
+                                                                <br>
+                                                                <input type="number" name="valor_abono" id="valor_abono" class="form-control" placeholder="precio del abono">
+                                                            </div>
+                                                            <div class="col-md-12 mt-3">
+                                                                <label for="valor_abono_2">Confirmar valor del abono</label>
+                                                                <br>
+                                                                <input type="number" name="valor_abono_2" id="valor_abono_2" class="form-control" placeholder="confirmar precio">
+                                                            </div>
+                                                            <div class="col-md-10 mt-3 mb-5">
+                                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                                                <button id="btnAddAbono" class="btn btn-primary">Agregar abono</button>
 
+                                                            </div>
+                                                        </div>
+                                                    </form>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                     </tbody>
                                     <tfoot>
                                         <tr>
@@ -216,11 +256,43 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="../../../view/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- sweet alert -->
     <script src="../../../view/admin/plugins/sweetalert2/sweetalert2.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+  <script>
+    $(document).ready(function() {
+        $(document).on('click', '.abrir-modal-abonos', function() {
+                const saldo_id = this.getAttribute('data-id-saldo'); // Obtiene el valor del atributo data-id
+                document.getElementById('saldo_id').value = saldo_id; // Asigna el valor al input
+            });
+    });
+    </script>
+       <script>
+       $(document).ready(function() {
+        // Escuchar el click del botón "Pagar"
+        $('#btnAddAbono').click(function(e) {
+            e.preventDefault(); // Evitar el envío del formulario por defecto
 
+            // Mostrar la alerta de confirmación
+            Swal.fire({
+                title: '¿Estás seguro que es el abono correcto?',
+                text: "Esta acción no se puede revertir y descontará al total del saldo pendiente.",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, confirmar',
+                cancelButtonText: 'Cancelar'
+            }).then((result) => {
+                // Si el usuario confirma, enviar el formulario
+                if (result.isConfirmed) {
+                    $('#formAddAbono').submit(); // Enviar el formulario
+                }
+            });
+        });
+
+    });
+</script>
     <?php
     $mensaje_error = $mensaje_error ?? ''; // Asegura que $mensaje_editar esté definido
-    $msj_eliminar = $msj_eliminar ?? ''; // Asegura que $mensaje_editar esté definido
+    $mensaje_ok = $mensaje_ok ?? ''; // Asegura que $mensaje_editar esté definido
     ?>
     <script>
     $(function() {
@@ -228,7 +300,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             toast: true,
             position: 'top-end',
             showConfirmButton: false,
-            timer: 5000,
+            timer: 7000,
             width: '80%',
             customClass: {
                 container: 'mi-clase-personalizada'
@@ -238,14 +310,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
         $('.swalDefaultSuccess').click(function() {
             Toast.fire({
                 icon: 'success',
-                title: '<?php echo $msj_eliminar; ?>'
+                title: '<?php echo $mensaje_ok; ?>'
 
             })
         });
         $('.swalDefaultInfo').click(function() {
             Toast.fire({
                 icon: 'info',
-                title: '<?php echo $msj_eliminar; ?>'
+                title: ''
             })
         });
         $('.swalDefaultError').click(function() {
@@ -271,11 +343,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script>
     $(document).ready(function() {
 
-        <?php if (isset($msj_eliminar) && !empty($msj_eliminar)) : ?>
+        <?php if (isset($mensaje_ok) && !empty($mensaje_ok)) : ?>
         // Simular un clic en el botón para activar el SweetAlert
 
 
-        $('#btnInfo').click();
+        $('#btnSuccess').click();
 
 
 
@@ -362,6 +434,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script>
     $(document).ready(function() {
         $('[data-toggle="tooltip"]').tooltip();
+        $(document).on('click', '.abrir-modal-abonos', function(event) {
+                event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+                var targetModal = $(this).data('target'); // Obtener el objetivo del modal
+                var idSaldo = $(this).data('id-saldo'); // Obtener el ID del saldo
+
+                // Aquí puedes agregar lógica adicional para usar el idSaldo si es necesario
+
+                $(targetModal).modal('show'); // Mostrar el modal
+            });
+       
     });
     </script>
 
