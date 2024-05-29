8d30bf5584fab0ec4395acf01d60ed8ca80c066c
daamaya22 <daamaya@unal.edu.co>
2024-05-29 15:32:26 -0500

    saldos pendientes y abonos terminado

--- view/admin/paginas/ventas/saldos_pendientes.php
+++ view/admin/paginas/ventas/saldos_pendientes.php
@@ -92,13 +92,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <section class="content">
 
                 <button id="btnInfo" type="button" class="btn btn-success swalDefaultSuccess" style="display:none ">
-                 
+
                 </button>
                 <button id="btnError" type="button" class="btn btn-success swalDefaultError" style="display:none ">
-             
+
                 </button>
                 <button id="btnSuccess" type="button" class="btn btn-success swalDefaultSuccess" style="display:none ">
-                 
+
                 </button>
                 <div class="container-fluid" style="max-width:1300px;">
 
@@ -120,32 +120,30 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                             <th scope="col">Id venta</th>
                                             <th scope="col">Nombres</th>
                                             <th scope="col">Valor venta</th>
-                                            <th scope="col">Valor abonado</th>
                                             <th scope="col">Saldo pendiente</th>
                                             <th scope="col">Ultimo abono</th>
-                                            <th scope="col">Agregar abono</th>
+                                            <th scope="col">Acciones</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <?php
-                                        if (isset($abonos_pendientes) and !empty($abonos_pendientes) ){
+                                        if (isset($abonos_pendientes) and !empty($abonos_pendientes)) {
 
                                             foreach ($abonos_pendientes as $abono) {
                                                 echo '<tr>
                       
                               <td>' . $abono['fecha_venta'] . '</td>
-                              <td>' . $abono['id_venta']. '</td>
+                              <td>' . $abono['id_venta'] . '</td>
                               <td>' . $abono['person_name'], " ", $abono['person_lastname'] . '</td>
                               <td>' . $abono['valor_venta'] . '</td>
-                              <td>' . $abono['valor_abonado'] . '</td>
                               <td>' . $abono['valor_venta'] - $abono['valor_abonado'] . '</td>
                               <td>' . $abono['ultimo_abono'] . '</td>
                               <td>
-                              <a class="btn btn-outline-success abrir-modal-abonos" data-target="#modalAbonos" data-id-saldo="'.$abono['id_saldo'].'" data-toggle="tooltip" data-placement=""top" title="Abonos realizados">
+                              <a class="btn btn-outline-success abrir-modal-abonos mr-2" data-target="#modalAbonos" data-id-saldo="' . $abono['id_saldo'] . '" data-toggle="tooltip" data-placement="top" title="Nuevo abono">
                               <i class="fas fa-plus"></i>
                               </a>
                           
-                              <a href="controller_detalle_abonos.php?id_venta=' . $abono['id_venta'] . '"   class="btn btn-primary" data-toggle="tooltip" title="Ver abonos realizados" id="desactivarVenta"><i  class="fas fa-eye"></i></a>
+                              <a href="controller_detalle_abonos.php?id_saldo=' . $abono['id_saldo'] . '"   class="btn btn-primary" data-toggle="tooltip" title="Ver abonos realizados" id="detalleSaldo"><i  class="fas fa-eye"></i></a>
                                
 
                               </td>
@@ -158,44 +156,53 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                         // Cierra la tabla HTML
 
                                         ?>
- <!-- Modal -->
- <div class="modal fade" id="modalAbonos" tabindex="-1" role="dialog" aria-labelledby="miModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h3 class="modal-title" id="miModalLabel">Nuevo abono</h3>
-                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                        <span aria-hidden="true">&times;</span>
-                                                    </button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <!-- Aquí irá el contenido dinámico del modal -->
-                                                    <!-- Agrega el ID "formPagar" al formulario -->
-                                                    <form id="formAddAbono" method="POST" action="">
-                                                        <div class="">
-                                                            <input type="hidden" name="saldo_id" id="saldo_id">
-                                                            <div class="col-md-12 mt-3">
-                                                                <label for="valor_abono">Valor del abono</label>
-                                                                <br>
-                                                                <input type="number" name="valor_abono" id="valor_abono" class="form-control" placeholder="precio del abono">
-                                                            </div>
-                                                            <div class="col-md-12 mt-3">
-                                                                <label for="valor_abono_2">Confirmar valor del abono</label>
-                                                                <br>
-                                                                <input type="number" name="valor_abono_2" id="valor_abono_2" class="form-control" placeholder="confirmar precio">
-                                                            </div>
-                                                            <div class="col-md-10 mt-3 mb-5">
-                                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                                                                <button id="btnAddAbono" class="btn btn-primary">Agregar abono</button>
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="modalAbonos" tabindex="-1" role="dialog"
+                                            aria-labelledby="miModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h3 class="modal-title" id="miModalLabel">Nuevo abono</h3>
+                                                        <button type="button" class="close" data-dismiss="modal"
+                                                            aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <!-- Aquí irá el contenido dinámico del modal -->
+                                                        <!-- Agrega el ID "formPagar" al formulario -->
+                                                        <form id="formAddAbono" method="POST" action="">
+                                                            <div class="">
+                                                                <input type="hidden" name="saldo_id" id="saldo_id">
+                                                                <div class="col-md-12 mt-3">
+                                                                    <label for="valor_abono">Valor del abono</label>
+                                                                    <br>
+                                                                    <input type="number" name="valor_abono"
+                                                                        id="valor_abono" class="form-control"
+                                                                        placeholder="precio del abono">
+                                                                </div>
+                                                                <div class="col-md-12 mt-3">
+                                                                    <label for="valor_abono_2">Confirmar valor del
+                                                                        abono</label>
+                                                                    <br>
+                                                                    <input type="number" name="valor_abono_2"
+                                                                        id="valor_abono_2" class="form-control"
+                                                                        placeholder="confirmar precio">
+                                                                </div>
+                                                                <div class="col-md-10 mt-3 mb-5">
+                                                                    <button type="button" class="btn btn-secondary"
+                                                                        data-dismiss="modal">Cerrar</button>
+                                                                    <button id="btnAddAbono"
+                                                                        class="btn btn-primary">Agregar abono</button>
 
+                                                                </div>
                                                             </div>
-                                                        </div>
-                                                    </form>
+                                                        </form>
 
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
                                     </tbody>
                                     <tfoot>
                                         <tr>
@@ -203,7 +210,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                             <th scope="col">Id venta</th>
                                             <th scope="col">Nombres</th>
                                             <th scope="col">Valor venta</th>
-                                            <th scope="col">Valor abonado</th>
+
                                             <th scope="col">Saldo pendiente</th>
                                             <th scope="col">Ultimo abono</th>
                                             <th scope="col">Agregar abono</th>
@@ -252,20 +259,23 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <!-- jQuery -->
     <script src="../../../view/admin/plugins/jquery/jquery.min.js"></script>
+    <!-- popper -->
+    <!-- <script src="../../../view/admin/plugins/popper/umd/popper.min.js"></script> -->
     <!-- Bootstrap 4 -->
     <script src="../../../view/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- sweet alert -->
     <script src="../../../view/admin/plugins/sweetalert2/sweetalert2.min.js"></script>
-  <script>
+    <script>
     $(document).ready(function() {
         $(document).on('click', '.abrir-modal-abonos', function() {
-                const saldo_id = this.getAttribute('data-id-saldo'); // Obtiene el valor del atributo data-id
-                document.getElementById('saldo_id').value = saldo_id; // Asigna el valor al input
-            });
+            const saldo_id = this.getAttribute(
+                'data-id-saldo'); // Obtiene el valor del atributo data-id
+            document.getElementById('saldo_id').value = saldo_id; // Asigna el valor al input
+        });
     });
     </script>
-       <script>
-       $(document).ready(function() {
+    <script>
+    $(document).ready(function() {
         // Escuchar el click del botón "Pagar"
         $('#btnAddAbono').click(function(e) {
             e.preventDefault(); // Evitar el envío del formulario por defecto
@@ -289,7 +299,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         });
 
     });
-</script>
+    </script>
     <?php
     $mensaje_error = $mensaje_error ?? ''; // Asegura que $mensaje_editar esté definido
     $mensaje_ok = $mensaje_ok ?? ''; // Asegura que $mensaje_editar esté definido
@@ -434,16 +444,17 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script>
     $(document).ready(function() {
         $('[data-toggle="tooltip"]').tooltip();
+
         $(document).on('click', '.abrir-modal-abonos', function(event) {
-                event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
-                var targetModal = $(this).data('target'); // Obtener el objetivo del modal
-                var idSaldo = $(this).data('id-saldo'); // Obtener el ID del saldo
+            event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+            var targetModal = $(this).data('target'); // Obtener el objetivo del modal
+            var idSaldo = $(this).data('id-saldo'); // Obtener el ID del saldo
 
-                // Aquí puedes agregar lógica adicional para usar el idSaldo si es necesario
+            // Aquí puedes agregar lógica adicional para usar el idSaldo si es necesario
 
-                $(targetModal).modal('show'); // Mostrar el modal
-            });
-       
+            $(targetModal).modal('show'); // Mostrar el modal
+        });
+
     });
     </script>
 
