31f21c77d3b571d0c86fb64b222c13f6ca02d72b
Martinhurtad0 <bmhurtadon@gmail.com>
2024-06-02 10:47:05 -0500

    cambios en modalidades
    quitar precio para docente

--- controller/admin/modalidades/controller_editar_modalidad.php
+++ controller/admin/modalidades/controller_editar_modalidad.php
@@ -13,12 +13,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $id = $_POST['id'];
     $price_hour_student = $_POST['price_hour_student'];
     $price_class_student = $_POST['price_class_student'];
-    $price_teacher = $_POST['price_teacher'];
+  
 
     $model = new buscar_modalidad_model();
     $datos_modalidad = $model->traer_datos_editar($id);
 
-    $rta_actualizar = $model->actualizar_modalidad($id, $price_hour_student, $price_teacher, $price_class_student);
+    $rta_actualizar = $model->actualizar_modalidad($id, $price_hour_student, $price_class_student);
     if ($rta_actualizar) {
         $msg = 'Modalidad actualizada con éxito!!!';
     }
