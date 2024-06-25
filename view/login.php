65fe7fe511dc5110fc4420564452082251c09108
amayadaniel677 <daamaya@unal.edu.co>
2024-06-25 17:01:04 -0500

    cambios produccion login

--- view/login.php
+++ view/login.php
@@ -9,11 +9,12 @@
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="../AdminLTE-3.2.0/plugins/fontawesome-free/css/all.min.css">
+    
+    <link rel="stylesheet" href="../view/admin/plugins/fontawesome-free/css/all.min.css">
     <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="../AdminLTE-3.2.0/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="../view/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="../AdminLTE-3.2.0/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="../view/admin/dist/css/adminlte.min.css">
 
     <style>
         body {
